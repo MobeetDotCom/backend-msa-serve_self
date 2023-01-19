36eb57933d071603c68462a055684f7377a3300f
RajeswarSharma <rjqwer47@gmail.com>
2023-01-19 23:51:53 +0530

    setup #1

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config({path:`.env.${process.env.NODE_ENV}`});
+const express = require("express");
+const logger = require("./configs/logger.config");
+
+const http = require("http");
+const app = express();
+const PORT = process.env.PORT || 8000 ;
+
+app.get("/ping",async(req,res)=>{ 
+    res.status(200).json({pid:process.pid});
+});
+
+const onError = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  switch (error.code) {
+    case "EACCES":
+      console.error(PORT + " requires elevated privileges");
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(PORT + " is already in use");
+      process.exit(1);
+    default:
+      throw error;
+  }
+};
+app.set("port", PORT);
+const server = http.createServer(app);
+server.on("error", onError);
+server.listen(PORT, () => {
+  logger.info(`server is listning at ${PORT}, instance_pid:${process.pid}`);
+  const server_info = {env:process.env.NODE_ENV,port:PORT,pid:process.pid,msa:process.env.msa}
+  console.log(server_info);
+});
+
